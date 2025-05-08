d8fc845c45bdbefe574ad1c09117b0ced4b29f25
Atiq Rahman <atiqcx@gmail.com>
2025-05-08 09:59:30 -0700

    Utilize Version Catalogs for version maintenance  this follows implementation from 'FinanceTool' project, ref commit  https://github.com/atiq-cs/FinanceTool/commit/a2c9d60503b2ca31b174f567a991b23d9dbb65be

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.jvm)
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
@@ -22,7 +22,7 @@ repositories {
 dependencies {
     // This dependency is used by the application.
     implementation(libs.guava)
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation(libs.kotlinx.coroutines.core)
 }
 
 testing {
@@ -30,7 +30,7 @@ testing {
         // Configure the built-in test suite
         val test by getting(JvmTestSuite::class) {
             // Use Kotlin Test test framework
-            useKotlinTest("2.0.20")
+            useKotlinTest(libs.versions.kotlin.get())
         }
     }
 }
@@ -38,7 +38,7 @@ testing {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(libs.versions.java.get().toInt())
     }
 }
 
